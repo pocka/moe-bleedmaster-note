6f9fac1338401af27e3229ceac93e3f7959fc9bb
pocka <pocka.moe@gmail.com>
2018-05-29 15:43:39 +0900

    :art: <Text/>をブラッシュアップ

--- src/components/atoms/Text/Text.stories.tsx
+++ src/components/atoms/Text/Text.stories.tsx
@@ -3,14 +3,27 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import { withInfo } from '@storybook/addon-info'
+import { select, text, withKnobs } from '@storybook/addon-knobs'
 
 import Text from './'
 
+const sizes = {
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+  'extra large': 'extra large'
+}
+
 storiesOf('atoms/Text', module)
+  .addDecorator(withKnobs)
   .add(
     'basic usage',
     withInfo(`
       基本となるテキストコンポーネント
-    `)(() => <Text>Foo</Text>)
+    `)(() => {
+      const size = select('Size', sizes, 'medium')
+      const label = text('Label', '吾輩は猫である')
+
+      return <Text size={size}>{label}</Text>
+    })
   )
-  .add('foo', () => <Text>Bar</Text>)
--- src/components/atoms/Text/index.tsx
+++ src/components/atoms/Text/index.tsx
@@ -1,5 +1,41 @@
 import * as React from 'react'
 
-const Text: React.SFC = ({ children }) => <p>{children}</p>
+import styled from 'styled-components'
+
+const sizeToPixel = (s: Required<Props>['size']): number => {
+  switch (s) {
+    case 'small':
+      return 18
+    case 'medium':
+      return 24
+    case 'large':
+      return 36
+    case 'extra large':
+      return 48
+  }
+}
+
+const Span = styled.span`
+  /* layouts */
+  font-size: ${(p: Required<Props>) => sizeToPixel(p.size)}px;
+
+  /* appearance */
+  color: #111;
+
+  /* misc */
+  font-family: 'Rounded Mplus 1c';
+`
+
+export interface Props {
+  /**
+   * フォントサイズ
+   * @default medium
+   */
+  size?: 'small' | 'medium' | 'large' | 'extra large'
+}
+
+const Text: React.SFC<Props> = ({ children, size = 'medium' }) => (
+  <Span size={size}>{children}</Span>
+)
 
 export default Text
